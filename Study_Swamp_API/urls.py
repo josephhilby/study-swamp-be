f8c014ab4ce2d51332ea963c4613b99395a66109
Joseph Hilby <joseph.hilby@gmail.com>
2025-06-01 15:17:25 -0500

    namespace all routes

--- Study_Swamp_API/urls.py
+++ Study_Swamp_API/urls.py
@@ -6,6 +6,6 @@ router = routers.DefaultRouter()
 router.register(r'user', views.UserViewSet)
 
 urlpatterns = [
-	path('', include(router.urls)),
+	path('api/v1/', include(router.urls)),
 	path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
