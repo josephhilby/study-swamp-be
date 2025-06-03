b6894de1fd39880137e5e0127f60229e2d602612
Joseph Hilby <joseph.hilby@gmail.com>
2025-06-03 18:34:53 -0500

    fixed issue with enum and JSON return

--- Study_Swamp_API/models.py
+++ Study_Swamp_API/models.py
@@ -4,12 +4,12 @@ class User(models.Model):
     class Meta:
         app_label = 'Study_Swamp_API'
 
-    class UserTypes(models.IntegerChoices):
-        STUDENT = 1, 'Student',
-        ADMIN = 2, 'Admin'
+    class UserTypes(models.TextChoices):
+        STUDENT = 'STUDENT', 'Student',
+        ADMIN = 'ADMIN', 'Admin'
 
     name = models.CharField(max_length=50)
-    user_type = models.IntegerField(
+    user_type = models.CharField(
         default=UserTypes.STUDENT,
         choices=UserTypes.choices
     )
