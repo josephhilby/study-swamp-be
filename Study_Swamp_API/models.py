011211eac660a23167929e5b8264cad4835f2cb5
Joseph Hilby <joseph.hilby@gmail.com>
2025-06-03 13:47:07 -0500

    update model and serializer

--- Study_Swamp_API/models.py
+++ Study_Swamp_API/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
-
     class Meta:
         app_label = 'Study_Swamp_API'
+
+    class UserTypes(models.IntegerChoices):
+        STUDENT = 1, 'Student',
+        ADMIN = 2, 'Admin'
+
+    name = models.CharField(max_length=50)
+    user_type = models.IntegerField(
+        default=UserTypes.STUDENT,
+        choices=UserTypes.choices
+    )
--- Study_Swamp_API/serializers.py
+++ Study_Swamp_API/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    user_type = serializers.SerializerMethodField()
     class Meta:
         model = User
         fields = '__all__'
+
+    def get_user_type(self, obj):
+        return obj.get_user_type_display()
