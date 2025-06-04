5c216ceb9f5958471b4374e545de585262a93755
Joseph Hilby <joseph.hilby@gmail.com>
2025-06-04 08:54:45 -0500

    add demo serializers

--- Study_Swamp_API/serializers.py
+++ Study_Swamp_API/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Group, Member, Meeting, Attendee, Comment
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['name', 'user_type']
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['name']
+
+class MemberSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    group = GroupSerializer(read_only=True)
+
+    class Meta:
+        model = Member
+        fields = ['user', 'group']
