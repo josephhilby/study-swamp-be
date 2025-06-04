59d1f60d8e09f6809019d3a3c666efd392916b25
Joseph Hilby <joseph.hilby@gmail.com>
2025-06-04 08:55:18 -0500

    add demo views

--- Study_Swamp_API/views.py
+++ Study_Swamp_API/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import User
-from .serializers import UserSerializer
+from .models import User, Group, Member
+from .serializers import UserSerializer, GroupSerializer, MemberSerializer
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
