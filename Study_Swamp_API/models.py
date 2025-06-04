7349eefa7121a99bd3ac7e8e88515b8a44996b95
Joseph Hilby <joseph.hilby@gmail.com>
2025-06-04 08:39:16 -0500

    add demo tables

--- Study_Swamp_API/models.py
+++ Study_Swamp_API/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class User(models.Model):
     class Meta:
@@ -14,3 +16,39 @@ class User(models.Model):
         default=UserTypes.STUDENT,
         choices=UserTypes.choices
     )
+
+class Group(models.Model):
+    class Meta:
+        app_label = 'Study_Swamp_API'
+
+    name = models.CharField(max_length=50)
+
+class Member(models.Model):
+    class Meta:
+        app_label = 'Study_Swamp_API'
+
+    user = models.ForeignKey(User, related_name='members', on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, related_name='members', on_delete=models.CASCADE)
+
+class Meeting(models.Model):
+    class Meta:
+        app_label = 'Study_Swamp_API'
+
+    name = models.CharField(max_length=50)
+    group = models.ForeignKey(Group, related_name='meetings', on_delete=models.CASCADE)
+
+class Attendee(models.Model):
+    class Meta:
+        app_label = 'Study_Swamp_API'
+
+    user = models.ForeignKey(User, related_name='attendees', on_delete=models.CASCADE)
+    meeting = models.ForeignKey(Meeting, related_name='attendees', on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    class Meta:
+        app_label = 'Study_Swamp_API'
+
+    text = models.TextField()
+    entity_id = models.PositiveIntegerField()
+    entity_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    entity_object = GenericForeignKey('entity_type', 'entity_id')
