705b9ea81e9608367570355b677a9d33fba74d47
Joseph Hilby <joseph.hilby@gmail.com>
2025-06-24 11:50:56 -0500

    make passwords write only

--- Study_Swamp_API/serializers.py
+++ Study_Swamp_API/serializers.py
@@ -4,9 +4,10 @@ from .models import User, Location, Group, Member, Meeting, Attendee, Badge, Awa
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
         extra_kwargs = {
             'is_superuser': {'read_only': True},
+            'password': {'write_only': True},
         }
 
     def to_representation(self, instance):
