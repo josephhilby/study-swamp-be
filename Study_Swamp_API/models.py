87f73dd0fb35a8f65c83835467de9105cb274509
Joseph Hilby <joseph.hilby@gmail.com>
2025-07-03 20:19:29 -0500

    add comments

--- Study_Swamp_API/models.py
+++ Study_Swamp_API/models.py
@@ -6,6 +6,12 @@ class User(AbstractUser):
     class Meta:
         app_label = 'Study_Swamp_API'
 
+    '''
+    POINTS:
+      - 5 for joining a group
+      - 5 for RSVP 'ACCEPTED' to a meeting
+      - 10 for marking checked_in to a meeting
+    '''
     points = models.IntegerField(default=0)
 
 
@@ -86,12 +92,12 @@ class Award(models.Model):
         app_label = 'Study_Swamp_API'
 
     class BadgeType(models.IntegerChoices):
-        EGG_TOOTH = 0, 'Egg Tooth'
-        FIRST_SPLASH = 1, 'First Splash'
-        SNAP_TO_IT = 2, 'Snap to It!'
-        TAILGATOR = 3, 'TailGATOR'
-        GATOR_DONE = 4, 'Gator Done'
-        CHOMP_CHAMP = 5, 'Chomp Champ'
+        EGG_TOOTH = 0, 'Egg Tooth'         # for creating an account
+        FIRST_SPLASH = 1, 'First Splash'   # for joining your first group
+        SNAP_TO_IT = 2, 'Snap to It!'      # for marking checked_in at first meeting
+        TAILGATOR = 3, 'TailGATOR'         # for marking checked_in at a meeting that has no associated group
+        GATOR_DONE = 4, 'Gator Done'       # for marking checked_in at 5 meetings
+        CHOMP_CHAMP = 5, 'Chomp Champ'     # for getting 100 points
 
 
     user = models.ForeignKey(User, related_name='awards', on_delete=models.CASCADE)
