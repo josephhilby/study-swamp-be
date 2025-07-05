9ed497075c2909e60c7fc6ca497ca3295b8ec6b9
Joseph Hilby <joseph.hilby@gmail.com>
2025-07-05 16:49:31 -0500

    update urls for auto docs

--- Study_Swamp_API/urls.py
+++ Study_Swamp_API/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from . import views
 
 router = routers.DefaultRouter()
@@ -14,6 +15,16 @@ router.register('meeting_comments', views.MeetingCommentViewSet)
 router.register('group_comments', views.GroupCommentViewSet)
 
 urlpatterns = [
+	# DRF API endpoints
 	path('api/v1/', include(router.urls)),
-	path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+	path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+	# Schema view (OpenAPI spec in JSON or YAML)
+	path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+	# Swagger UI (uses schema endpoint)
+	path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+	# ReDoc UI (also uses schema endpoint)
+	path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
