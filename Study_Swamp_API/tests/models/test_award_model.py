e4bf179e6b71a79982fb51ef4d4cc03db617771c
Joseph Hilby <joseph.hilby@gmail.com>
2025-07-13 14:15:58 -0500

    add award model tests

---
+++ Study_Swamp_API/tests/models/test_award_model.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from Study_Swamp_API.models import Award
+from Study_Swamp_API.tests.factories import AwardFactory, UserFactory
+
+
+class MemberAwardTests(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.award = AwardFactory(user=self.user)
+
+    def test_award_instance(self):
+        self.assertIsInstance(self.award, Award)
+
+    def test_award_state(self):
+        assert self.award.user == self.user
