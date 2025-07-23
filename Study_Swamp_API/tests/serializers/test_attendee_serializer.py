b7689b8df75858ecad800e1c1d1ddd58df53ada5
Joseph Hilby <joseph.hilby@gmail.com>
2025-07-23 14:30:41 -0500

    add tests

--- Study_Swamp_API/tests/serializers/test_attendee_serializer.py
+++ Study_Swamp_API/tests/serializers/test_attendee_serializer.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from Study_Swamp_API.tests.factories import AttendeeFactory, UserFactory, MeetingFactory
+from rest_framework.test import APIClient
+from Study_Swamp_API.models import Attendee
+from Study_Swamp_API.tests.factories import AttendeeFactory, UserFactory, MeetingFactory, GroupFactory, LocationFactory
 from Study_Swamp_API.serializers import AttendeeSerializer
 
 
@@ -19,17 +21,60 @@ class TestSerializerAttendee(TestCase):
         assert isinstance(data['creator'], bool)
         assert isinstance(data['editor'], bool)
 
-    def test_attendee_create(self):
+    def test_attendee_auto_create(self):
+        user = UserFactory()
+        group = GroupFactory()
+        location = LocationFactory()
+        meeting = MeetingFactory()
+
+        client = APIClient()
+        client.force_authenticate(user=user)
+
+        response = client.post('/api/v1/meetings/', {
+            'group': group.id,
+            'location': location.id,
+            'name': meeting.name,
+            'start_time': meeting.start_time,
+            'end_time': meeting.end_time
+        }, format='json')
+
+        assert response.status_code == 201
+
+        attendee = Attendee.objects.get(user=user)
+        assert attendee.creator == True
+        user.refresh_from_db()
+        assert user.points == 150
+
+    def test_attendee_update(self):
         user = UserFactory()
         meeting = MeetingFactory()
-        assert user.points == 0
 
         data = {
             'user': user.id,
-            'group': meeting.id,
+            'meeting': meeting.id,
+            'checked_in': False
         }
         attendee = AttendeeSerializer(data=data)
         assert attendee.is_valid(), attendee.errors
-        attendee.save()
+        attendee = attendee.save()
+
+        assert attendee.checked_in == False
+        assert user.points == 0
+
+        update = {
+            'user': user.id,
+            'checked_in': True
+        }
+
+        updated_attendee = AttendeeSerializer(
+            attendee,
+            data=update,
+            partial=True
+        )
+
+        assert updated_attendee.is_valid(), updated_attendee.errors
+        updated_attendee.save()
+
         user.refresh_from_db()
-        assert user.points == 150
+        assert user.points == 100
+
